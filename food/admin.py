566ca529211af3050ef770590297a81d8d47c731
Ryan McLaughlin <ryanmclaughlin17@hotmail.com>
2024-04-16 16:29:52 +0100

    all_models_created_for_food

--- food/admin.py
+++ food/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Bakery_Item
+from .models import Bakery_Item, Sandwiches_and_Prepared_Food, Pastry_Item, Egg_Bite, Porridge_Item
 # Register your models here.
 admin.site.register(Bakery_Item)
+admin.site.register(Sandwiches_and_Prepared_Food)
+admin.site.register(Pastry_Item)
+admin.site.register(Egg_Bite)
+admin.site.register(Porridge_Item)
--- food/models.py
+++ food/models.py
@@ -12,3 +12,45 @@ class Bakery_Item(models.Model):
 
     def __str__(self):
         return self.name
+
+class Sandwiches_and_Prepared_Food(models.Model):
+    name = models.CharField(max_length=50)
+    sku = models.IntegerField(default=0)
+    ingredients = models.TextField(max_length=1500)
+    vegan = models.BooleanField(default=False)
+    vegetarian = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+    
+class Pastry_Item(models.Model):
+    name = models.CharField(max_length=50)
+    sku = models.IntegerField(default=0)
+    ingredients = models.TextField(max_length=1500)
+    vegan = models.BooleanField(default=False)
+    vegetarian = models.BooleanField(default=False)
+    time_in_box = models.CharField(max_length=20, default=False)
+    time_in_pastry_case = models.CharField(max_length=20, default=False)
+
+    def __str__(self):
+        return self.name
+    
+class Egg_Bite(models.Model):
+    name = models.CharField(max_length=50)
+    sku = models.IntegerField(default=0)
+    ingredients = models.TextField(max_length=1500)
+    vegan = models.BooleanField(default=False)
+    vegetarian = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+    
+class Porridge_Item(models.Model):
+    name = models.CharField(max_length=50)
+    sku = models.IntegerField(default=0)
+    ingredients = models.TextField(max_length=1500)
+    vegan = models.BooleanField(default=False)
+    vegetarian = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
