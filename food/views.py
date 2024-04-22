513ae624a8e23b4ad0f7f2946845aea09a6a453d
Ryan McLaughlin <ryanmclaughlin17@hotmail.com>
2024-04-22 13:05:33 +0100

    dropdown_menu_added

--- food/views.py
+++ food/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Bakery_Item, Sandwiches_and_Prepared_Food, Pastry_Item, Egg_Bite, Porridge_Item
+
 # Create your views here.
 
 def food_home(request):
@@ -13,23 +13,3 @@ def waste(request):
 
 def ordering(request):
     return render(request, 'food/ordering.html')
-
-def bakery_item(request):
-    bakery_item = Bakery_Item.objects.all().order_by('name')
-    return render(request, 'food/sales.html', {'bakery_items':bakery_item})
-
-def sandwiches_and_prepared_food(request):
-    sandwiches_and_prepared_food = Sandwiches_and_Prepared_Food.objects.all().order_by('name')
-    return render(request, 'food/sales.html', {'sandwiches_and_prepared_food':sandwiches_and_prepared_food})
-
-def pastry_item(request):
-    pastry_item = Pastry_Item.objects.all().order_by('name')
-    return render(request, 'food/sales.html', {'pastry_item':pastry_item})
-
-def egg_bite(request):
-    egg_bite = Egg_Bite.objects.all().order_by('name')
-    return render(request, 'food/sales.html', {'egg_bite':egg_bite})
-
-def porridge_item(request):
-    porridge_item = Porridge_Item.objects.all().order_by('name')
-    return render(request, 'food/sales.html', {'porridge_item':porridge_item})
